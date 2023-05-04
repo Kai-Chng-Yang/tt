fc71750180bd5c16bbf27ef153d3c9ed912ad679
Kai-Chng-Yang <71475667+Kai-Chng-Yang@users.noreply.github.com>
2023-05-04 06:09:35 +0000

    modify

---
+++ app.py
@@ -0,0 +1,14 @@
+
+
+#執行 flask --app app run --debug
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.jinja.html")
+
+@app.route("/learning/")
+def learning():
+    return render_template("learning.jinja.html")
